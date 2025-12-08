3a16ef476b163f56742a4a96e7cf74d651869d42
James Stakebake <james.stakebake@protonmail.com>
2025-12-08 14:05:14 -0500

    build(packages): add steam to configuration

--- software/custom/starship.sh
+++ software/custom/starship.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-curl -sS https://starship.rs/install.sh | sh
+# Check if starship is installed
+if ! command -v starship &> /dev/null; then
+    echo "Installing Starship"
+    
+    # Install starship custom
+    curl -sS https://starship.rs/install.sh | sh
+    
+    if [ $? -eq 0 ]; then
+        echo "Starship has been successfully installed."
+    else
+        echo "Failed to install Starship. Please check your internet connection or repository configuration."
+        exit 1
+    fi
+else
+    echo "Skipping: Starship is already installed"
+fi   
